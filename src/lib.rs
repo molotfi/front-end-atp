327a340819a04a1e394bf84b1076d82c846e1e69
Mateusz Belka <belka.mateusz.bm@gmail.com>
2021-05-10 16:53:28 +0200

    update on rust webassembly transition from compiler rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,51 @@
+mod utils;
+use utils::log;
+
 use wasm_bindgen::prelude::*;
+use serde::{Deserialize, Serialize};
+use meancop::main;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Query {
+    #[serde(default = "default_bool")]
+    reduction: bool,
+
+    #[serde(default = "default_bool")]
+    extension: bool,
+
+    #[serde(default = "default_bool")]
+    extension_inc: bool,
+
+    #[serde(default = "default_bool")]
+    extension_exc: bool,
+
+    #[serde(default = "default_limit_depth")]
+    limit_search_depth: i32,
+
+    #[serde(default = "default_bool")]
+    conjunctive: bool,
+
+    #[serde(default = "default_bool")]
+    sort: bool,
+
+    problem: String,
+}
+
+fn default_bool() -> bool {
+    false
+}
+
+fn default_limit_depth() -> i32 {
+    1
+}
 
 #[wasm_bindgen]
-pub fn helloworld(input : String) -> String { input }
+pub fn run(data : &str) {
+    /*
+     * TODO: error handling
+     * possible solution is to return serde_json::Result;
+     * that way the frontend would be able to print error message if one occurs.
+     */
+    let json : Query = serde_json::from_str(data).unwrap();
+    log(&*format!("{:?}", json));
+}
