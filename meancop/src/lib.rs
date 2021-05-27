6373256af750cb87a02b0eb975211a52bb51c529
Mateusz Belka <belka.mateusz.bm@gmail.com>
2021-05-27 22:18:35 +0200

    better error handling + remove unnecessary code

--- meancop/src/lib.rs
+++ meancop/src/lib.rs
@@ -1,10 +1,8 @@
-mod cli;
 mod query;
 mod error;
 pub mod parse;
 pub mod preprocess;
 pub mod start;
 
-pub use cli::Cli;
 pub use query::Query;
 pub use error::Error;
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 use wasm_bindgen::prelude::*;
 use meancop::start::main;
+extern crate console_error_panic_hook;
 
 
 #[wasm_bindgen]
 pub fn run(data : &str) -> String {
+    init_panic_hook();
     main(data)
+}
+
+#[wasm_bindgen]
+pub fn init_panic_hook() {
+    console_error_panic_hook::set_once();
 }
